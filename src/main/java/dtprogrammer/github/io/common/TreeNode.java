b5f6aab68554214802bcb629c61d83007699791e
DuctTape Programmer <ducttapeprogrammer@gmail.com>
2017-08-21 10:21:15 -0700

    Add TreeNode class

---
+++ src/main/java/dtprogrammer/github/io/common/TreeNode.java
@@ -0,0 +1,14 @@
+package dtprogrammer.github.io.common;
+
+/**
+ * Leetcode TreeNode class for testing and dedbugging
+ */
+public class TreeNode {
+    public final int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}
