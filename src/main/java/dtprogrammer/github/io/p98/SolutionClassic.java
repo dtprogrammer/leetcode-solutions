73e5b2a9123c8de1d828af1c40eab7ea4afe1611
DuctTape Programmer <ducttapeprogrammer@gmail.com>
2017-09-06 00:27:38 -0700

    98. Validate Binary Search Tree

--- src/main/java/dtprogrammer/github/io/p98/SolutionClassic.java
+++ src/main/java/dtprogrammer/github/io/p98/SolutionClassic.java
@@ -13,49 +13,33 @@ import dtprogrammer.github.io.common.TreeNode;
  * The right subtree of a node contains only nodes with keys greater than the node's key.
  * Both the left and right subtrees must also be binary search trees.
  * Example 1
- *  2
+ * 2
  * / \
  * 1   3
  * Binary tree [2,1,3], return true.
  * Example 2:
- *  1
+ * 1
  * / \
  * 2   3
  * Binary tree [1,2,3], return false.
- * <p>
+ * This approach is the classic approach to solve this problem although it depends on Integer.MIN_VALUE and Integer
+ * .MAX_VALUE which is not very nice. See other solutions.
  * Time complexity: O(n)
  */
-public class Solution {
-
+public class SolutionClassic {
     public boolean isValidBST(TreeNode root) {
-        TreeNodeWrapper prev = new TreeNodeWrapper(null);
-        return isValidBST(root, prev);
+        return isValidBST(root, null, null);
     }
 
-    private boolean isValidBST(TreeNode root, TreeNodeWrapper prev) {
+    private boolean isValidBST(TreeNode root, Integer low, Integer high) {
         if (root == null) {
             return true;
         }
 
-        if (!isValidBST(root.left, prev)) {
+        if ((low != null && root.val <= low) || (high != null && root.val >= high)) {
             return false;
         }
 
-        if (prev.node != null && root.val <= prev.node.val) {
-            return false;
-        }
-        prev.node = root;
-        return isValidBST(root.right, prev);
-    }
-
-    /**
-     * A wrapper class around TreeNode to retain the value across recursive call
-     */
-    private class TreeNodeWrapper {
-        TreeNode node;
-
-        TreeNodeWrapper(TreeNode node) {
-            this.node = node;
-        }
+        return isValidBST(root.left, low, root.val) && isValidBST(root.right, root.val, high);
     }
 }
